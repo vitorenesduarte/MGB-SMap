05cde6c5f97078990158b7fbca6cf3a02361bc32
otrack <0track@gmail.com>
2018-04-30 19:06:51 +0200

    fix localtesting.sh

--- localTesting.sh
+++ localTesting.sh
@@ -23,21 +23,21 @@ echo ${zkAddress}
 
 echo "Starting MGB"
 
-docker run --rm --net host --cpus=0.1 -e "ZK=${zkAddress}"\
+docker run --rm --net host -e "ZK=${zkAddress}"\
     -e "ID=0" \
     -e "NODE_NUMBER=3" \
     -e "HPORT=5000" \
     -e "CPORT=6000" \
     -p 6000:6000 ${image} >& mgb1.txt &
 
-docker run --rm --net host --cpus=0.1 -e "ZK=${zkAddress}" \
+docker run --rm --net host -e "ZK=${zkAddress}" \
     -e "ID=1" \
     -e "NODE_NUMBER=3" \
     -e "HPORT=5001" \
     -e "CPORT=6001" \
     -p 6001:6001 ${image} >& mgb2.txt &
 
-docker run --rm --net host --cpus=0.1 -e "ZK=${zkAddress}" \
+docker run --rm --net host -e "ZK=${zkAddress}" \
     -e "ID=2" \
     -e "NODE_NUMBER=3" \
     -e "HPORT=5002" \
@@ -52,30 +52,30 @@ while [ ${up} != 3 ]; do
              wc -l)
 done
 
-#echo "Starting SMAP"
-#docker run --rm --net host -e "ZHOST=${localHost}"\
-#    -e "ZPORT=5000" \
-#    -e "SERVERPORT=8980" \
-#    -e "RETRIES=400" \
-#    -e "VERBOSE=false" \
-#    -e "STATIC=true" \
-#    tfr011/mgb-smap:latest &> smap1.txt &
-#
-#docker run --rm --net host -e "ZHOST=${localHost}"\
-#    -e "ZPORT=5001" \
-#    -e "SERVERPORT=8981" \
-#    -e "VERBOSE=false" \
-#    -e "RETRIES=400" \
-#    -e "STATIC=true" \
-#    tfr011/mgb-smap:latest &> smap2.txt &
-#
-#up=0
-#while [ ${up} != 2 ]; do
-#    sleep 1
-#    up=$(cat smap*.txt |
-#             grep "MGB-SMap Server started" |
-#             wc -l)
-#done
+echo "Starting SMAP"
+docker run --rm --net host -e "ZHOST=${localHost}"\
+   -e "ZPORT=5000" \
+   -e "SERVERPORT=8980" \
+   -e "RETRIES=400" \
+   -e "VERBOSE=false" \
+   -e "STATIC=true" \
+   tfr011/mgb-smap:latest &> smap1.txt &
+
+docker run --rm --net host -e "ZHOST=${localHost}"\
+   -e "ZPORT=5001" \
+   -e "SERVERPORT=8981" \
+   -e "VERBOSE=false" \
+   -e "RETRIES=400" \
+   -e "STATIC=true" \
+   tfr011/mgb-smap:latest &> smap2.txt &
+
+up=0
+while [ ${up} != 2 ]; do
+   sleep 1
+   up=$(cat smap*.txt |
+            grep "MGB-SMap Server started" |
+            wc -l)
+done
 
 echo "Will sleep forever"
 while true; do sleep 10000; done
